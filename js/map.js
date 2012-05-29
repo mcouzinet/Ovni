020703570152e5cef7fa4d6eac45ec562ef18c74
cedricmarteau <cedric.marteau@gmail.com>
2012-05-29 17:05:42 +0200

    reglages zoom

--- js/map.js
+++ js/map.js
@@ -59,7 +59,7 @@ document.onkeyup = function(e){
 		 case 90: //Bouton Z - Zoom -
 			zoom=0;
 		 break;
-		 case 90: //Bouton Z - Zoom -
+		 case 65: //Bouton Z - Zoom -
 			zoom=0;
 		 break;
 	}
@@ -78,37 +78,60 @@ document.onkeydown = function(e){
 	var lngmouton = point.getLongitude();
 	var distance = Math.sqrt(Math.pow(latcenter-latmouton,2)+Math.pow(lngcenter-lngmouton,2));
 	//Déplacement sur la carte
-	switch (e.keyCode) {
-	 	case 65: //Bouton A - Zoom +
-			// lookAt.setRange(lookAt.getRange() / 8.0);
-			// 			ge.getView().setAbstractView(lookAt);
-			//console.log('zoom'+LookAt.getRange());
-			zoom = 100;
-			// lookAt.setRange(lookAt.getRange() * zoom);
-			// 			ge.getView().setAbstractView(lookAt);
-		 break;
-		 case 90: //Bouton Z - Zoom -
-			// lookAt.setRange(lookAt.getRange() * 8.0);
-			// 			ge.getView().setAbstractView(lookAt);
-			zoom = -100;
-			// lookAt.setRange(lookAt.getRange() * zoom);
-			// 			ge.getView().setAbstractView(lookAt);
-		 break;
-		 case 38: //Bouton Up
-			bougeX = 0.005;
-		 break;
-		 case 40: //Bouton Down
-			bougeX = -0.005;
-		 break;
-		 case 37: //Bouton Left
-			bougeY = -0.005;
-		 break;
-		 case 39: //Bouton Right
-			bougeY = 0.005;
-		 break;
-		case 32: //Bouton Espace
-			sendToActionScript('espace');
-		break;
+		 if (camera.getAltitude()>250000)
+		{
+			switch (e.keyCode) {
+			case 65: //Bouton A - Zoom -
+				zoom = 10000;
+			 break;
+			 case 90: //Bouton Z - Zoom +
+				zoom = -10000;
+			 break;
+			 case 38: //Bouton Up
+				//console.log("camera.getAltitude()"+camera.getAltitude());
+				bougeX = 0.2;
+			 break;
+			 case 40: //Bouton Down
+				bougeX = -0.2;
+			 break;
+			 case 37: //Bouton Left
+				bougeY = -0.2;
+			 break;
+			 case 39: //Bouton Right
+				bougeY = 0.2;
+			 break;
+			case 32: //Bouton Espace
+				sendToActionScript('espace');
+			break;
+			}
+		}
+		else{
+			switch (e.keyCode) {
+			case 65: //Bouton A - Zoom -
+				zoom = 1000;
+			 break;
+			 case 90: //Bouton Z - Zoom +
+				zoom = -1000;
+				//lookAt.setRange(lookAt.getRange() / 5000);
+				// 			ge.getView().setAbstractView(lookAt);
+			 break;
+			 case 38: //Bouton Up
+				console.log("camera.getAltitude()"+camera.getAltitude());
+				bougeX = 0.005;
+			 break;
+			 case 40: //Bouton Down
+				bougeX = -0.005;
+			 break;
+			 case 37: //Bouton Left
+				bougeY = -0.005;
+			 break;
+			 case 39: //Bouton Right
+				bougeY = 0.005;
+			 break;
+			case 32: //Bouton Espace
+				sendToActionScript('espace');
+			break;
+			}	
 	 }
 		enterFrame();
 }
@@ -127,14 +150,6 @@ var enterFrame = function (){
 function initCB(instance) {
    	ge = instance;
   	ge.getWindow().setVisibility(true);
-	// console.log('yep');
-	// 	placemark = ge.createPlacemark('');
-	//   	var point = ge.createPoint('');
-	//   	point.setLatitude(37);
-	//   	point.setLongitude(-122);
-	//   	placemark.setGeometry(point);
-	//   	placemark.setName('Drag Me!');
-	//   	ge.getFeatures().appendChild(placemark);
 
   	// Look at the placemark we created.
 	var la = ge.createLookAt('');
